a36099902669e7374a35634d96b0a42b0672b8c8
Abedalrhman Nsasra <evernote.4ever@gmail.com>
2022-03-13 02:26:59 +0200

    docs security + add logs

--- src/main/java/com/example/simpleserver/controllers/LogController.java
+++ src/main/java/com/example/simpleserver/controllers/LogController.java
@@ -1,11 +1,15 @@
 package com.example.simpleserver.controllers;
 
 import com.example.simpleserver.exceptions.InternalServerError;
+import com.example.simpleserver.exceptions.UnauthorizedException;
 import com.example.simpleserver.models.TimeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,34 +19,27 @@ import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
 
+@Tag(name = "Log", description = "Managing the logs")
 @RestController
 @RequestMapping("/api")
 public class LogController {
     private final Log logger = LogFactory.getLog(LogController.class);
 
-    @GetMapping("/time/info")
+    @Operation(summary = "Get and log current time, no authorization require")
+    @GetMapping("/time")
     public TimeResponse addInfoLog() {
         final String now = Date.from(Instant.now()).toString();
         logger.info(String.format("Info time requested at %s", now));
         return new TimeResponse(now);
     }
 
-    @GetMapping("/time/error")
-    public TimeResponse addErrorLog() {
-        final String now = Date.from(Instant.now()).toString();
-        logger.error(String.format("Error time requested at %s", now));
-        return new TimeResponse(now);
-    }
-
-    @GetMapping("/time/warn")
-    public TimeResponse addWarnLog() {
-        final String now = Date.from(Instant.now()).toString();
-        logger.warn(String.format("Warn time requested at %s", now));
-        return new TimeResponse(now);
-    }
-
+    @Operation(summary = "Get All logs as a text, authorization is required")
     @GetMapping("/log")
-    public String getLogs() {
+    public String getLogs(@RequestHeader(value = "Authorization", required = false) String authorization) {
+        final String accessKey = System.getenv("ACCESS_KEY");
+        if (authorization == null || accessKey == null || !authorization.equals("Bearer " + accessKey)) {
+            throw new UnauthorizedException("Unauthorized access");
+        }
         try {
             return FileUtils.readFileToString(new File("logs/out.log"), StandardCharsets.UTF_8);
         }
